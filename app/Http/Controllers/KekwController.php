785a7b20190f8a69040fd2d52c31f9de136f9fff
Ivan Kopylov <i-kopilov@mail.ru>
2022-05-05 16:20:10 +0300

    kekw

---
+++ app/Http/Controllers/KekwController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class KekwController extends Controller
+{
+    public function index()
+    {
+        return 'kekw';
+    }
+}
